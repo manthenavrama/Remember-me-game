9699e2a5be3fe31381deea5672a60881c82b9a25
Varma Manthena <vmanthena@corp.paradigmit.com>
2016-09-29 12:10:26 +0530

    import code

---
+++ app/app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by vmanthena on 27-Sep-16.
+ */
+
+var rememberGameApp = angular.module('rememberGameApp', []);
+
+rememberGameApp.factory('game', function() {
+    var tileNames = ['AUDI', 'BMW', 'HONDA', 'FORD', 'SKODA', 'SUZUKI',
+        'NISSAN', 'JAGUAR'];
+    return new Game(tileNames);
+});
+
+rememberGameApp.controller('GameCtrl', function GameCtrl($scope, game) {
+    $scope.game = game;
+});
+
+rememberGameApp.directive('card', function() {
+    return {
+        template: '<div class="container">' +
+        '<div class="card" ng-class="{flipped: tile.flipped}">' +
+        '<img  class="front" ng-src="img/back.jpg">' +
+        '<img  class="back" ng-src="img/{{tile.title}}.jpg">' +
+        '</div>' +
+        '</div>'
+    }
+});
